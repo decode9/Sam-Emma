4c203b42e46cec3aba6ba393b8849dd293c3c6d0
Victor Caceres <caceres.vc95@gmail.com>
2021-01-30 04:02:04 -0400

    Create random spawner enemies

---
+++ Assets/Script/NPC/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public float waitTime;
+    public float startWaitTime;
+
+    public Transform moveSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                waitTime = startWaitTime;
+            } 
+            else 
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/Script/NPC/RandomSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    private List<Vector3> enemyPositions = new List<Vector3>();
+
+    void Start() {
+        InvokeRepeating("ShowEnemy", 2f, 3f);
+    }
+
+    private void ShowEnemy()
+    {
+        int randEnemy = Random.Range(0, enemyPrefabs.Length);
+        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        Vector3 pointCoordinates = spawnPoints[randSpawnPoint].position;
+
+        if (!verifyEnemyPosition(pointCoordinates))
+            Instantiate(enemyPrefabs[randEnemy], pointCoordinates, transform.rotation);
+
+        enemyPositions.Add(pointCoordinates);
+    }
+
+    private bool verifyEnemyPosition(Vector3 coordinates)
+    {
+        return enemyPositions.Exists(position => position == coordinates);
+    }
+}
