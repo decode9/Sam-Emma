e03ea1f7efe120e73ad200e4eb154a01b3ded0d0
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-01-31 00:35:06 -0400

    introduction

---
+++ Assets/Script/UI/SecondScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class SecondScreen : MonoBehaviour
+{
+    public UnityEvent onInteraction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitScreen());
+    }
+
+
+    IEnumerator WaitScreen()
+    {
+        yield return new WaitForSeconds(10);
+        onInteraction.Invoke();
+    }
+}
